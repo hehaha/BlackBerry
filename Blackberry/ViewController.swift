a6d72a02c430307dd77a4ebe3aa3334f1ea71c91
hexin <hexin@ricebook.com>
2016-02-09 22:32:01 +0800

    init project

---
+++ Blackberry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blackberry
+//
+//  Created by 何鑫 on 16/2/9.
+//  Copyright © 2016年 何鑫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
